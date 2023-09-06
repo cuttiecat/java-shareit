6daebc069c2c9ab6ed1266b3098ff8c46d4e1de0
cuttiecat <godrdinasvetlana@yandex.ru>
2023-09-06 09:09:42 +0400

    15 ТЗ. Подсмотрела решение...

---
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -0,0 +1,34 @@
+package ru.practicum.shareit.request;
+
+import lombok.*;
+import ru.practicum.shareit.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "requests", schema = "public")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ItemRequest {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requester_id", nullable = false)
+    private User requester;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+}
---
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.user;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users", schema = "public")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+}
